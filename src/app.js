f55c87be59e9b6f9fce8c42ebe1e16e3b313af63
Siddhartha Sharma <siddhartha1234sharma@gmail.com>
2025-03-21 01:56:08 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -13,5 +13,12 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use.apply(express.static("public"))
 
+//routes
+
+import userRouter from './routes/user.routes.js'
+
+
+app.use("/api/v1/users", userRouter)
+
 
 export {app} 
