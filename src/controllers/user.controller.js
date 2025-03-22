a58b9a447b3bb33b56b1c476062be43de991732b
Siddhartha Sharma <siddhartha1234sharma@gmail.com>
2025-03-22 22:04:21 +0530

    add getWatchHistory

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,8 +5,6 @@ import {uploadOnCloudinary} from '../utils/cloudinary.js';
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from 'jsonwebtoken'
 import mongoose from "mongoose";
-// import { validateHeaderName } from "http";
-// import { error } from "console"; 
 
 const generateAccessAndRefreshTokens = async(userId) => {
   try {
@@ -30,7 +28,8 @@ const registerUser = asyncHandler( async (req, res)=>
 
   if (
     [fullName, email, username, password ].some((field) => field?.trim() === "")
-  ) {
+  ) 
+  {
     throw new ApiError(400,"All fields are mandatory")
   }
 
@@ -71,8 +70,6 @@ const user = await User.create({
   password,
   username: username.toLowerCase()
 })
-
-// console.log(user);
 
 const createdUser = await User.findById(user._id).select(
   "-password -refreshToken"
@@ -254,7 +251,7 @@ const updateAccountDetails = asyncHandler(async(req,res)=> {
     throw new ApiError(400, "All fields are required")
   }
 
-  const user = User.findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: {
@@ -333,7 +330,132 @@ const updateCoverImage = asyncHandler(async(req, res) =>
     )
 })
 
-export {
+const gerUserChannelProfile = asyncHandler(async(req, res)=> {
+  if(!username?.trim()) {
+    throw new ApiError(400, "Username is missing")
+  }
+const channel = await User.aggregate([
+  {
+    $match: {
+      username: username?.toLowerCase()
+    }
+  },
+  {
+    $lookup: {
+      from: "subscriptions",
+      localField: "_id",
+      foreignField: "channel",
+      as : "subscribers"
+    }
+  },
+  {
+    $lookup: {
+      from: "subscriptions",
+      localField: "_id",
+      foreignField: "subscriber",
+      as : "subscribedTo"
+    }
+  },
+  {
+    $addFields : {
+      subscribersCount : {
+        $size:"$subscribers"
+      },
+      channelsSubscribedToCount: {
+        $size: "$subscribedTo"
+      },
+      isSubscribed: {
+        $cond : {
+          if : {$in: [req.user?._id, "$subscribers.subscriber"]},
+          then: true,
+          else: false
+        }
+      }
+    }
+  },
+  {
+    $project : {
+      fullName: 1,
+      username: 1,
+      subscribersCount: 1,
+      channelsSubscribedToCount: 1,
+      isSubscribed: 1,
+      avatar: 1,
+      coverImage: 1,
+      email: 1
+    }
+  }
+])
+
+console.log(channel);
+
+if(!channel?.length){
+  throw new ApiError(404, "channel does not exists")
+}
+
+return res
+.status(200)
+.json(
+    new ApiResponse(200, channel[0], "User Channel Fetched Successfully")
+)
+})
+
+const getWatchHistory = asyncHandler( async(req,res)=> {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    }, 
+    {
+      $lookup : {
+        from: "videos",
+        localField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner"
+              }
+            }
+          }
+
+        ]
+      }
+    }
+  ])
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200,
+      user[0].watchHistory,
+      "Watch History Fetched succesfully"
+    )
+  )
+})
+
+export 
+{
   registerUser,
   loginUser,
   logoutUser,
@@ -342,9 +464,10 @@ export {
   getCurrentPassword,
   updateAccountDetails,
   updateUserAvatar,
-  updateCoverImage
+  updateCoverImage,
+  gerUserChannelProfile,
+  getWatchHistory
 }
-
 
 // Algorithms :
 // get user details from frontend
